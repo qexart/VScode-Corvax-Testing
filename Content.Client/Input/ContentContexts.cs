25367c587b189b3b12500f278504abfd77adfe63
bananchiki <87230164+Darkiich@users.noreply.github.com>
2025-08-10 14:57:04 +0300

    [NEW MECHANICS] Поднятие на руки (#1738) <!-- Что вы изменили в этом пулл-реквесте? --> Кнопка активации - пробел Была добавлена новая механика - взятие сущности на руки. Брать можно всех, но кроме КПБ. КПБ может взять только КПБ. <!-- Почему оно было изменено и как изменение повлияет на игру и её баланс. -->
    Чтобы было
    <!-- Если речь идет об изменении кода, кратко изложите на высоком уровне
    принцип работы нового кода. Перечислите все критические изменения,
    включая изменения пространства имён, публичных классов/методов/полей-
    -->
    - [x] Изменения были протестированы на локальном сервере, и всё работает
    отлично.
    - [x] PR закончен и требует просмотра изменений.
    <!--Вставьте медиа, демонстрирующее изменения, если это требуется-->
    https://youtu.be/WBQsg_wLGGg
    
    :cl: Darkiich
    - add: Добавлена новая механика - поднятие на руки
    
    ---------
    
    Co-authored-by: Schrödinger <132720404+Schrodinger71@users.noreply.github.com>

--- Content.Client/Input/ContentContexts.cs
+++ Content.Client/Input/ContentContexts.cs
@@ -91,6 +91,7 @@ namespace Content.Client.Input
             human.AddFunction(ContentKeyFunctions.Arcade1);
             human.AddFunction(ContentKeyFunctions.Arcade2);
             human.AddFunction(ContentKeyFunctions.Arcade3);
+            human.AddFunction(ContentKeyFunctions.PickupHumans); // ADT-Tweak
 
             // actions should be common (for ghosts, mobs, etc)
             common.AddFunction(ContentKeyFunctions.OpenActionsMenu);
--- Content.Client/Options/UI/Tabs/KeyRebindTab.xaml.cs
+++ Content.Client/Options/UI/Tabs/KeyRebindTab.xaml.cs
@@ -185,7 +185,7 @@ namespace Content.Client.Options.UI.Tabs
             AddButton(ContentKeyFunctions.MoveStoredItem);
             AddButton(ContentKeyFunctions.RotateStoredItem);
             AddButton(ContentKeyFunctions.SaveItemLocation);
-
+            AddButton(ContentKeyFunctions.PickupHumans); // ADT-Tweak
             AddHeader("ui-options-header-interaction-adv");
             AddButton(ContentKeyFunctions.SmartEquipBackpack);
             AddButton(ContentKeyFunctions.SmartEquipBelt);
