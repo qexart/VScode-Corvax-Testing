a32fcd9f44cbb4c5adf9849a73e74677a87f8e69
Arthur <Krabsburger111110@gmail.com>
2026-02-04 14:24:29 +0700

    Фикс поднятия на руки, фикс стаков медицины, ПДА с /tg/station.

--- Content.Shared/ADT/PickupHumans/Systems/SharedPickupHumansSystem.cs
+++ Content.Shared/ADT/PickupHumans/Systems/SharedPickupHumansSystem.cs
@@ -16,7 +16,6 @@ using Content.Shared.Hands.EntitySystems;
 using Content.Shared.ADT.Components.PickupHumans;
 using Content.Shared.Movement.Systems;
 using Content.Shared.Mobs;
-using Content.Shared.ADT.Crawling;
 using Content.Shared.Popups;
 using Content.Shared.ADT.Silicon;
 using Content.Shared.Movement.Pulling.Systems;
@@ -113,10 +112,10 @@ public abstract class SharedPickupHumansSystem : EntitySystem
         if (HasComp<PickupingHumansComponent>(args.User) || HasComp<TakenHumansComponent>(args.Target))
             return;
 
-        if (HasComp<CrawlingComponent>(args.User))
+        if (TryComp<StandingStateComponent>(uid, out var stand) && !stand.Standing)
             return;
 
-        if (handComp.CountFreeHands() < component.HandsRequired || handComp.CountFreeHands() > component.HandsRequired)
+        if (handComp.Count < component.HandsRequired || handComp.Count > component.HandsRequired)
             return;
 
         AlternativeVerb verb = new()
@@ -163,7 +162,7 @@ public abstract class SharedPickupHumansSystem : EntitySystem
     /// </summary>
     private void StartPickupDoAfter(EntityUid uid, EntityUid target, PickupHumansComponent comp)
     {
-        if (HasComp<CrawlingComponent>(uid))
+        if (TryComp<StandingStateComponent>(uid, out var stand) && !stand.Standing)
             return;
 
         if (HasComp<PickupingHumansComponent>(target))
@@ -273,7 +272,7 @@ public abstract class SharedPickupHumansSystem : EntitySystem
 
     private bool CanPickup(EntityUid uid, EntityUid target, PickupHumansComponent? pickupComp)
     {
-        if (HasComp<CrawlingComponent>(uid))
+        if (TryComp<StandingStateComponent>(uid, out var stand) && !stand.Standing)
             return false;
 
         if (uid == target)
@@ -293,7 +292,7 @@ public abstract class SharedPickupHumansSystem : EntitySystem
         if (!HasComp<PickupHumansComponent>(uid) || !HasComp<PickupHumansComponent>(target))
             return false;
 
-        if (handComp.CountFreeHands() != pickupComp.HandsRequired)
+        if (handComp.Count != pickupComp.HandsRequired)
         {
             _popup.PopupEntity(Loc.GetString("popup-hands-required"), uid, uid);
             return false;
